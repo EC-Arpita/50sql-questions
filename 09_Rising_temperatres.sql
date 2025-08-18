b94bda37be0d8f752790ff55b74c547ee0f693ff
Arpita Thakur <155470521+EC-Arpita@users.noreply.github.com>
2025-08-18 14:04:20 +0530

    Add files via upload

---
+++ 09_Rising_temperatres.sql
@@ -0,0 +1,22 @@
+-- LeetCode Problem: Rising Temperature
+-- Write a solution to find all dates' id with higher temperatures compared 
+-- to its previous dates (yesterday).
+-- Return the result table in any order.
+
+-- Table: Weather
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the column with unique values for this table.
+-- There are no different rows with the same recordDate.
+
+SELECT 
+    w1.id
+FROM Weather w1
+JOIN Weather w2
+    ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
